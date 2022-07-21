5c6912586f77e0e63f556f4be21b5461566ca4d9
Farid.Bagirov <farid.bagirov@jetbrains.com>
2022-07-21 19:17:20 +0300

    update far counter with commit history

--- Counter/FaRCounter.py
+++ Counter/FaRCounter.py
@@ -3,53 +3,84 @@ from collections import defaultdict
 from Counter.CounterBase import CounterBase
 
 
-# TODO add commits
 class FaRCounter(CounterBase):
-    """
-    Files at Risk metric. Calculates number of files in a time frame that are known to at most one active contributer
-    """
-    @classmethod
-    def count(cls, history, from_date=None, to_date=None):
+    def __init__(self, data):
+        self.when_known = defaultdict(lambda: {})
+        self.when_left = defaultdict(lambda: None)
+        self.when_created = defaultdict(lambda: None)
+        self.data = data
+        self.prepare()
+        self.wkr = []
+
+    def prepare(self):
+
+        for i, review in self.data.pulls.iterrows():
+            file = review.file_path
+            reviewer = review.reviewer_login
+
+            if reviewer not in self.when_left:
+                self.when_left[reviewer] = review['created_at']
+            else:
+                self.when_left[reviewer] = max(self.when_left[reviewer], review['created_at'])
+
+            if file not in self.when_created:
+                self.when_created[file] = review['created_at']
+            else:
+                self.when_created[file] = min(self.when_created[file], review['created_at'])
+
+        for i, commit in self.data.commits.iterrows():
+            file = commit.key_file
+            user = commit.key_user
+
+            if user not in self.when_known[file]:
+                self.when_known[file][user] = commit['date']
+            else:
+                prev = self.when_known[file][user]
+                self.when_known[file][user] = min(commit['date'], prev)
+
+            if file not in self.when_created:
+                self.when_created[file] = commit['date']
+            else:
+                self.when_created[file] = min(self.when_created[file], commit['date'])
+
+            if user not in self.when_left:
+                self.when_left[user] = commit['date']
+            else:
+                self.when_left[user] = max(self.when_left[user], commit['date'])
+
+    def count(self, history, from_date=None, to_date=None):
         if from_date is None:
             from_date = history[0]['date']
         if to_date is None:
             to_date = history[-1]['date']
 
-        when_created = defaultdict(lambda: None)
-        when_left = defaultdict(lambda: None)
-        is_known = defaultdict(lambda: set())
-
+        when_known_rev = defaultdict(lambda: {})
         for review in history:
             for file in review['file_path']:
-                if file not in when_created:
-                    when_created[file] = review['date']
-                else:
-                    when_created[file] = min(when_created[file], review['date'])
-
                 for reviewer in review['reviewer_login']:
-                    is_known[file].add(reviewer)
-
-            for reviewer in review['reviewer_login']:
-                if reviewer not in when_left:
-                    when_left[reviewer] = review['date']
-                else:
-                    when_left[reviewer] = max(when_left[reviewer], review['date'])
+                    if reviewer not in when_known_rev[file]:
+                        when_known_rev[file][reviewer] = review['date']
+                    else:
+                        prev = when_known_rev[file][reviewer]
+                        when_known_rev[file][reviewer] = min(review['date'], prev)
 
         active_dev = {}
-        for review in history:
-            if review['date'] > to_date:
-                break
+        for file in self.when_created:
+            if self.when_created[file] > to_date:
+                continue
+            if file not in active_dev:
+                active_dev[file] = set()
+            for user in self.when_known[file]:
+                if self.when_left[user] >= to_date > self.when_known[file][user]:
+                    active_dev[file].add(user)
 
-            for file in review['file_path']:
-                if file not in active_dev:
-                    active_dev[file] = set()
-                for reviewer in is_known[file]:
-                    if when_left[reviewer] >= to_date:
-                        active_dev[file].add(reviewer)
+            for user in when_known_rev[file]:
+                if self.when_left[user] >= to_date > when_known_rev[file][user]:
+                    active_dev[file].add(user)
+
         active_dev = {file: len(active_dev[file]) for file in active_dev}
         far = len([file for file in active_dev if active_dev[file] <= 1])
 
-        return far
+        self.wkr.append(when_known_rev)
 
-
-
+        return far, active_dev
