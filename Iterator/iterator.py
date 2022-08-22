074c09e1f084421c9240df7dba6ba2763cc1dcaa
Farid.Bagirov <farid.bagirov@jetbrains.com>
2022-08-22 13:40:37 +0300

    add new stream iterator for all models

--- Iterator/iterator.py
+++ Iterator/iterator.py
@@ -25,10 +25,65 @@ class IteratorBase(ABC):
         self.dataset.replace(data, cur_rec)
 
 
+class StreamIteratorBase(IteratorBase, ABC):
+    def __init__(self, dataset):
+        super().__init__(dataset)
+        self.data = dataset.data
+
+    def __iter__(self):
+        self.ind = 0
+        return self
+
+    def __next__(self):
+        if self.ind + 1 >= len(self.data):
+            raise StopIteration
+
+        from_id = self.ind
+        self.ind = self.get_next()
+
+        train = self.data[from_id: self.ind + 1]
+        test = self.data[self.ind + 1]
+
+        self.ind += 1
+        return train, test
+
+    @abstractmethod
+    def get_next(self):
+        pass
+
+    def replace(self, data, rev):
+        data = deepcopy(data)
+        rev_name = self.dataset.get_revname()
+        l = len(data[rev_name])
+        data[rev_name][np.random.randint(l)] = rev
+
+        return data
+
+
+class StreamUntilIterator(StreamIteratorBase):
+    def __init__(self, dataset, until_type='pull'):
+        super().__init__(dataset)
+        self.until_type = until_type
+
+    def get_next(self):
+        try:
+            while self.data[self.ind + 1]['type'] != self.until_type:
+                self.ind += 1
+        except IndexError:
+            raise StopIteration
+
+
+class StreamAllIterator(StreamIteratorBase):
+    def get_next(self):
+        return self.ind
+
+
+# TODO update to StreamIterator or remove
 class BatchIterator(IteratorBase):
     """
     implementation of the iterator that iterates over batches of consecutive reviews performed in some range of time
     """
+
     def __init__(self, dataset, initial_delta, test_interval):
         """
         :param dataset: data over which iterates
@@ -71,33 +126,3 @@ class BatchIterator(IteratorBase):
         if self.to_date > self.end_date:
             raise StopIteration
         return train, test
-
-
-class OneByOneIterator(IteratorBase):
-    """
-    implementation of a iterator that iterates over reviews one by one
-    """
-    def __init__(self, dataset):
-        super().__init__(dataset)
-        self.data = dataset.data.to_dict('records')
-
-    def __iter__(self):
-        self.ind = 0
-        return self
-
-    def __next__(self):
-        if self.ind + 1 >= len(self.data):
-            raise StopIteration
-        train = self.data[self.ind]
-        test = self.data[self.ind + 1]
-
-        self.ind += 1
-        return train, test
-
-    def replace(self, data, rev):
-        data = deepcopy(data)
-        rev_name = self.dataset.get_revname()
-        l = len(data[rev_name])
-        data[rev_name][np.random.randint(l)] = rev
-
-        return data
