9c868d349eab20526f507d31f54b0ce1c91cab66
Farid.Bagirov <farid.bagirov@jetbrains.com>
2022-10-19 14:35:52 +0300

    add results saving function

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import numpy as np
@@ -13,3 +14,23 @@ def get_df(path):
             continue
     return dfs
 
+
+def save_results(path, results, model):
+    try:
+        with open(path, 'r') as _:
+            pass
+    except FileNotFoundError:
+        with open(path, 'w') as _:
+            pass
+    try:
+        with open(path, "r") as fp:
+            data = json.load(fp)
+    except json.JSONDecodeError:
+        with open(path, 'w') as fp:
+            json.dump({type(model).__name__: results}, fp)
+        return
+
+    data[type(model).__name__] = results
+
+    with open(path, "w") as fp:
+        json.dump(data, fp)
