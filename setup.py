4a99f1fdf409fa8b809f9f7a425c4f0bfff82782
kraalfar <kraalfar@gmail.com>
2023-01-12 11:49:18 +0200

    test setup for autodoc generation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='batcore',
+      version='0.1',
+      description="batcore",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['batcore'],
+      zip_safe=False,
+      install_requires=['numpy', 'scipy', 'tqdm'],
+      )
