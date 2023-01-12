34db40e35f467c1759ec95da56baf4b360d79946
kraalfar <kraalfar@gmail.com>
2023-01-12 15:31:15 +0200

    revert conf.py change

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 # -- Project information
@@ -19,17 +20,18 @@ extensions = [
     'sphinx.ext.autodoc',
 ]
 
-# intersphinx_mapping = {
-#     'python': ('https://docs.python.org/3/', None),
-#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-# }
-# intersphinx_disabled_domains = ['std']
-#
-# templates_path = ['_templates']
-#
-# # -- Options for HTML output
-#
-# html_theme = 'sphinx_rtd_theme'
-#
-# # -- Options for EPUB output
-# epub_show_urls = 'footnote'
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
