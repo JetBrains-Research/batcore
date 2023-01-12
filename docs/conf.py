d9bda8dab86fc10cde81ef82d40bbe2953abba13
kraalfar <kraalfar@gmail.com>
2023-01-12 11:59:53 +0200

    test conf for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 # -- Project information
 
 project = 'BaTCoRe'
